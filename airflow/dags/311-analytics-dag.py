f0ed6c3bcbb79cae8543ba0c26946963a4452bdd
Emmanuel Gligbe <emmanuelgligbe246@gmail.com>
2024-06-07 12:08:14 +0000

    new file:   311 Service Requests Tableau Workbook.twb 	modified:   airflow/airflow-scheduler.err 	modified:   airflow/airflow-scheduler.log 	modified:   airflow/airflow-scheduler.out 	modified:   airflow/airflow-scheduler.pid 	modified:   airflow/airflow.cfg 	modified:   airflow/dags/311-analytics-dag.py 	modified:   airflow/dags/__pycache__/311-analytics-dag.cpython-312.pyc

--- airflow/dags/311-analytics-dag.py
+++ airflow/dags/311-analytics-dag.py
@@ -1,7 +1,5 @@
-
 from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.utils.dates import days_ago
 import requests
 import json
 from airflow.operators.python import PythonOperator
@@ -14,12 +12,11 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 s3_bucket = '311-cleaned-bucket'
 raw_s3_bucket = '311-raw-bucket'
 
-
 # Create a unique timestamp for file naming
 now = datetime.now()
 dt_now_string = now.strftime("%d%m%Y%H%M%S")
 
-# Calculate the date range
+# Calculate the date range for the past 7 days
 today = datetime.now()
 seven_days_ago = today - timedelta(days=7)
 
@@ -27,6 +24,7 @@ seven_days_ago = today - timedelta(days=7)
 today_str = today.strftime('%Y-%m-%dT%H:%M:%S')
 seven_days_ago_str = seven_days_ago.strftime('%Y-%m-%dT%H:%M:%S')
 
+# Function to extract 311 data from the NYC Open Data API
 def extract_311_data(**kwargs):
     url = kwargs['url']
     dt_string = kwargs['date_string']
@@ -34,7 +32,7 @@ def extract_311_data(**kwargs):
     response = requests.get(url)
     response_data = response.json()
     
-    # specify the output file path
+    # Specify the output file path
     output_file_path = f"/home/ubuntu/response_data_{dt_string}.json"
     file_str = f'response_data_{dt_string}.csv'
     
@@ -44,19 +42,22 @@ def extract_311_data(**kwargs):
         
     print(f"Data saved to {output_file_path}")
 
+    # Return the file paths
     output_list = [output_file_path, file_str]
     return output_list
 
+# Default arguments for the DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(1),
+    'start_date': datetime(2024, 6, 6),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=1)
 }
 
+# Define the DAG
 with DAG(
     dag_id='extract_311_data_dag',
     default_args=default_args,
@@ -64,6 +65,7 @@ with DAG(
     catchup=True
 ) as dag:
 
+    # Task to extract 311 data
     extract_311_data_task = PythonOperator(
         task_id='extract_311_data',
         python_callable=extract_311_data,
@@ -73,11 +75,13 @@ with DAG(
         }
     )
     
+    # Task to load the extracted data to S3
     load_to_s3_task = BashOperator(
         task_id='load_to_s3',
         bash_command='aws s3 mv {{ ti.xcom_pull(task_ids="extract_311_data")[0] }} s3://311-raw-bucket/',
     )
     
+    # Task to check if the file is available in S3
     is_file_in_s3_available_task = S3KeySensor(
         task_id='is_file_in_s3_available',
         bucket_key='{{ ti.xcom_pull("extract_311_data")[1] }}',
@@ -88,6 +92,7 @@ with DAG(
         poke_interval=20,
     )
 
+    # Task to create the staging table in Redshift
     create_staging_table_task = PostgresOperator(
         task_id='create_staging_table',
         postgres_conn_id='redshift_conn_id',
@@ -116,6 +121,7 @@ with DAG(
         """
     )
         
+    # Task to transfer data from S3 to Redshift staging table
     transfer_s3_to_redshift_staging_task = S3ToRedshiftOperator(
         task_id='transfer_s3_to_redshift_staging',
         aws_conn_id='aws_connection_id',
@@ -127,6 +133,7 @@ with DAG(
         copy_options=['csv IGNOREHEADER 1'],        
     )
 
+    # Task to delete existing records from the target table
     delete_existing_records_task = PostgresOperator(
         task_id='delete_existing_records',
         postgres_conn_id='redshift_conn_id',
@@ -137,6 +144,7 @@ with DAG(
         """,
     )
 
+    # Task to insert new records from the staging table to the target table
     insert_new_records_task = PostgresOperator(
         task_id='insert_new_records',
         postgres_conn_id='redshift_conn_id',
@@ -145,17 +153,19 @@ with DAG(
             SELECT * FROM sr.staging_service_requests;
         """,
     )
-    
-    delete_csv_from_s3_task = BashOperator(
-    task_id='delete_csv_from_s3',
-    bash_command='aws s3 rm s3://311-cleaned-bucket/{{ ti.xcom_pull("extract_311_data")[1] }}',
-    )
-
+     
+    # Task to truncate the staging table
     truncate_staging_table_task = PostgresOperator(
         task_id='truncate_staging_table',
         postgres_conn_id='redshift_conn_id',
         sql="TRUNCATE TABLE sr.staging_service_requests;",
     )
+    
+    # Task to delete the CSV file from S3 after processing
+    delete_csv_from_s3_task = BashOperator(
+        task_id='delete_csv_from_s3',
+        bash_command='aws s3 rm s3://311-cleaned-bucket/{{ ti.xcom_pull("extract_311_data")[1] }}',
+    )
 
 # Set the task dependencies
-extract_311_data_task >> load_to_s3_task >> is_file_in_s3_available_task >> create_staging_table_task >> transfer_s3_to_redshift_staging_task >> delete_existing_records_task >> insert_new_records_task >> delete_csv_from_s3_task >> truncate_staging_table_task
+extract_311_data_task >> load_to_s3_task >> is_file_in_s3_available_task >> create_staging_table_task >> transfer_s3_to_redshift_staging_task >> delete_existing_records_task >> insert_new_records_task >> truncate_staging_table_task >> delete_csv_from_s3_task
